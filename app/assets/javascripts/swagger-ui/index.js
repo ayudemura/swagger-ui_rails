a3992d355489beb84caa9d73f30a34a990efec0f
Ayu Demura <ayu.demura@gmail.com>
2017-01-09 14:56:45 -0500

    Update swagger to 2.2.10

--- app/assets/javascripts/swagger-ui/index.js
+++ app/assets/javascripts/swagger-ui/index.js
@@ -1,13 +1,17 @@
-//= require ./lib/shred.bundle.js
+//= require ./lib/es5-shim.js
+//= require ./lib/highlight.9.1.0.pack_extended.js
+//= require ./lib/highlight.9.1.0.pack.js
+//= require ./lib/js-yaml.min.js
+//= require ./lib/jsoneditor.min.js
+//= require ./lib/lodash.min.js
+//= require ./lib/marked.js
+//= require ./lib/object-assign-pollyfill.js
+//= require ./lib/sanitize-html.min.js
+//= require ./lib/swagger-oauth.js
 //= require ./lib/jquery-1.8.0.min.js
 //= require ./lib/jquery.slideto.min.js
 //= require ./lib/jquery.wiggle.min.js
 //= require ./lib/jquery.ba-bbq.min.js
-//= require ./lib/handlebars-1.0.0.js
-//= require ./lib/underscore-min.js
+//= require ./lib/handlebars-4.0.5.js
 //= require ./lib/backbone-min.js
-//= require ./lib/swagger.js
-//= require ./lib/swagger-client.js
 //= require ./swagger-ui.js
-//= require ./lib/highlight.7.3.pack.js
-
